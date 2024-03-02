911c91680833dbf2062a4c9b292e4e96589e850f
Bartosz Masztalerz <bartosz.masztalerz@gmail.com>
2024-03-02 22:53:38 +0100

    containers

--- variables.tf
+++ variables.tf
@@ -2,9 +2,43 @@
 variable "proxmox_node" {
   type = string
 }
+variable "homelab_domain" {
+  type    = string
+  default = "homelab.local"
+}
+
+# ssh
+variable "vm_template_cloud_init_ssh_key_path" {
+  type    = string
+  default = "~/.ssh/id_ed25519.pub"
+}
 
 # data stores
 variable "datastore_container_template" {
   type    = string
   default = "local"
+}
+variable "datastore_container_disk" {
+  type    = string
+  default = "local-lvm"
+}
+
+# containers
+variable "containers_definition" {
+  type = list(object({
+    vm_id        = number
+    hostname     = string
+    description  = string
+    unprivileged = bool
+    disk_size    = number
+    mount_points = list(object({
+      volume = string
+      path   = string
+    }))
+    mac_address      = string
+    dns_servers      = list(string)
+    cpu_cores        = number
+    memory_dedicated = number
+    tags             = list(string)
+  }))
 }
